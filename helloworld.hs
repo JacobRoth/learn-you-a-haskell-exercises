0ab79475446cda7849ac6b177b6961bcf79a368a
Jacob Roth <jroth@hmc.edu>
2015-07-22 10:55:21 -0500

    wrote something cool into my little test program.

--- helloworld.hs
+++ helloworld.hs
@@ -1,1 +1,25 @@
-main = putStrLn "hello world"
+{-
+
+main = do
+        line <- getLine
+        if (null line)
+                then do
+                        putStrLn "Halting"
+                        --return ()
+                else do
+                        putStrLn $ reverseWords line
+                        main
+-}
+
+
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
+
+printList :: Show a => [a] -> IO ()
+printList = doListOfIO . map print
+
+doListOfIO :: [IO ()] -> IO ()
+doListOfIO [] = return ()
+doListOfIO (x:xs) = do
+        x
+        doListOfIO xs
