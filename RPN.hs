84371e7a7d6239df446772ae85fdae134450d0e4
Jacob Roth <jroth@hmc.edu>
2015-07-27 11:05:30 -0500

    worked a little more on my RPN calculator function, reversing the stack order.

--- RPN.hs
+++ RPN.hs
@@ -1,11 +1,12 @@
-data StackItem = Number Double | BinOp BinaryOperator | UnOp UnaryOperator deriving (Show, Eq)
+data StackItem = Number Double | BinOp BinaryOperator | UnOp UnaryOperator | StackOp StackOperator deriving (Show, Eq)
 data BinaryOperator = Plus | Minus | Times | Divide | Exponent deriving (Show,Eq)
-data UnaryOperator = Sqrt | LogE | LogTen deriving (Show, Eq)
+data UnaryOperator = Sqrt | Ln | LogTen deriving (Show, Eq)
+data StackOperator = Sum | Product deriving (Show,Eq)
 
-type Stack = [StackItem] -- rpn stack
+type Stack = [StackItem] -- rpn stack. We store the stack backwards, I.E. the top of the stack is the head of the list. This means pushing/popping the stack is done with consing to the head, or stuff near the head.
 
-processStackOnePass :: Stack -> Stack
-processStackOnePass ((Number n1):(Number n2):(BinOp someoperator):xs) = ((Number result): xs)
+crunchStack :: Stack -> Stack
+crunchStack ((BinOp someoperator):(Number n2):(Number n1):xs) = crunchStack ((Number result): xs)
     where
         result = case someoperator of
             Plus     -> n1+n2
@@ -14,18 +15,21 @@ processStackOnePass ((Number n1):(Number n2):(BinOp someoperator):xs) = ((Number
             Divide   -> n1/n2
             Exponent -> n1 ** n2
 
-processStackOnePass ((Number n):(UnOp someoperator):xs) = processStack ((Number result):xs)
+crunchStack ((UnOp someoperator):(Number n):xs) = crunchStack ((Number result): xs)
     where
         result = case someoperator of
             Sqrt   -> sqrt n
-            LogE   -> log n
+            Ln     -> log n
             LogTen -> logBase 10 n
-            
-processStackOnePass (x:xs) = x:(processStackOnePass xs) 
-processStackOnePass [] = [] -- base case
 
-processStack stack
-    | newstack == stack = newstack
-    | otherwise         = processStack newstack
+crunchStack ((StackOperator someoperator):xs) = crunchStack $ f xs
     where
-        newstack = processStackOnePass stack
+        f = case someoperator of
+            Sum -> sum
+            Product -> product
+
+crunchStack (x:xs) = x:(crunchStack xs)
+crunchStack []  = []  -- base case
+
+solveStack :: Stack -> Stack
+solveStack = foldr (\x acc -> crunchStack (x:acc)) []
